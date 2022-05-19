f28d561e5e1b81fbf40b089b871d3dcb7041063d
VinniLuiz <104585339+VinniLuiz@users.noreply.github.com>
2022-05-19 11:41:30 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
-const num = document.querySelectorAll(".controle")
+// let controles = document.querySelectorAll(".controle")
+// controles.forEach(element => {
+//   element.querySelectorAll(".controle-ajuste").forEach(element2 => {
+//     element2.addEventListener('click', () => {
+//       if(element2.innerHTML == "+") {
+//         valor = parseInt(element.querySelector(".controle-contador").value)
+//         valor += 1
+//         if(valor >= 0 && valor <= 10){
+//           element.querySelector(".controle-contador").value = valor
+//         }
+//         else{
+//           valor = parseInt(element.querySelector(".controle-contador").value)
+//           valor -= 1
+//           if(valor >= 0 && valor <= 10){
+//             element.querySelector(".controle-contador").value = valor
+//           }
+//         }
+//       }
+//     })
+//   })
+// });
 
-controle.forEach(element => {
-  element.querySelectorAll(".controle-ajuste").forEach(element2 => {
-    element2.addEventListener('click', () => {
-      if(element2.innerHTML == "+") {
-        num = parseInt(element.querySelector(".controle-contador").value)
-        num += 1
-        if(num >= 0 && num <= 10){
-          element.querySelector(".controle-contador").value = num
-        }
-        else{
-          valor = parseInt(element.querySelector(".controle-contador").value)
-          valor -= 1
-          if(num >= 0 && num <= 10){
-            element.querySelector(".controle-contador").value = num
-          }
-        }
-      }
-    })
-  })
-});
+const btnBracoMenos = document.getElementById("btnBracoMenos")
+const btnBracoMais = document.getElementById("btnBracoMais")
+
+btnBracoMenos.addEventListener("click", diminui)
+btnBracoMais.addEventListener("click", aumenta)
+
+function diminui(){
+  const txtBraco = document.getElementById("txtBraco")
+  txtBraco.value = txtBraco.value - 1
+}
+
+function aumenta(){
+  const txtBraco = document.getElementById("txtBraco")
+  txtBraco.value = parseInt(txtBraco.value) + 1
+}
