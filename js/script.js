ed4bab8a20d3227211da1acb654f1433776d77bf
VinniLuiz <104585339+VinniLuiz@users.noreply.github.com>
2022-05-24 11:45:03 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-const btnBracoMenos = document.getElementById("btnBracoMenos")
-const btnBracoMais = document.getElementById("btnBracoMais")
-
-btnBracoMenos.addEventListener("click", diminui)
-btnBracoMais.addEventListener("click", aumenta)
-btnBlindMenos.addEventListener("click", removeBlind)
-btnBlindMais.addEventListener("click", addBlind)
-btnNucleoMais.addEventListener("click", addNuc)
-btnNucleoMenos.addEventListener("click", removeNuc)
-
+/*------------------------Braços---------------------------------*/
+btnBracoMenos.addEventListener("click", diminui);
+btnBracoMais.addEventListener("click", aumenta);
 
 function diminui(){
   const txtBraco = document.getElementById("txtBraco");
@@ -15,7 +8,6 @@ function diminui(){
     txtBraco.value = txtBraco.value - 1;
   }
 }
-
 function aumenta(){
   const txtBraco = document.getElementById("txtBraco");
   if (txtBraco.value >= 0 && txtBraco.value < 10){
@@ -23,13 +15,16 @@ function aumenta(){
   }
 }
 
+/*------------------Blindagem-------------------------*/
+btnBlindMenos.addEventListener("click", removeBlind)
+btnBlindMais.addEventListener("click", addBlind)
+
 function addBlind(){
   const txtBlind = document.getElementById("txtBlind");
   if(txtBlind.value >= 0 && txtBlind.value < 10){
     txtBlind.value = parseInt(txtBlind.value) + 1;
   }
 }
-
 function removeBlind(){
   const txtBlind = document.getElementById("txtBlind");
   if(txtBlind.value <=10 && txtBlind.value > 0){
@@ -37,16 +32,53 @@ function removeBlind(){
   }
 }
 
+/*------------------Nucleo---------------------------*/
+btnNucleoMais.addEventListener("click", addNuc);
+btnNucleoMenos.addEventListener("click", removeNuc);
+
 function addNuc() {
   const txtNucleo = document.getElementById("txtNucleo");
   if(txtNucleo.value >= 0 && txtNucleo.value < 10){
     txtNucleo.value = parseInt(txtNucleo.value) + 1;
   }
 }
-
 function removeNuc() {
   const txtNucleo = document.getElementById("txtNucleo");
   if(txtNucleo.value <= 10 && txtNucleo.value > 0){
     txtNucleo.value = parseInt(txtNucleo.value) - 1;
+  }
+}
+
+/*-----------------------Pernas----------------------*/
+btnPernocasMais.addEventListener("click", addPernocas);
+btnPernocasMenos.addEventListener("click", removePernocas);
+
+function addPernocas() {
+  const txtPernocas = document.getElementById("txtPernocas");
+  if(txtPernocas.value >= 0 && txtPernocas.value < 10){
+    txtPernocas.value = parseInt(txtPernocas.value) + 1;
+  }
+}
+function removePernocas() {
+  const txtPernocas = document.getElementById("txtPernocas");
+  if(txtPernocas.value > 0 && txtPernocas.value <= 10){
+    txtPernocas.value = parseInt(txtPernocas.value) - 1;
+  }
+}
+
+/*--------------------------Foguetes--------------------------*/
+btnFogoMais.addEventListener("click", addFogo);
+btnFogoMenos.addEventListener("click", removeFogo);
+
+function addFogo() {
+  const txtFogo = document.getElementById("txtFogo");
+  if(txtFogo.value >= 0 && txtFogo.value < 10){
+    txtFogo.value = parseInt(txtFogo.value) + 1
+  }
+}
+function removeFogo() {
+  const txtFogo = document.getElementById("txtFogo");
+  if(txtFogo.value <= 10 && txtFogo.value > 0){
+    txtFogo.value = parseInt(txtFogo.value) - 1;
   }
 }
